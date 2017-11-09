ef4e06dced76dbd032dec537d271ae2a3e6be174
Valentijn <valentijnvermeij@gmail.com>
2017-11-09 22:41:37 +0100

    Update ruleupdate.js

--- commands/moderation/ruleupdate.js
+++ commands/moderation/ruleupdate.js
@@ -42,12 +42,6 @@ module.exports = class createRules extends Command {
 	async run(msg, {rulehash}) {
 		let rulesChannel = this.client.channels.get(config.rulesChannel); //Rules channel is obtained by ID
 		let newRules = this.client.rules.get(rulehash);
-		let messagescount = rulesChannel.
-		await function () {
-			if (newRules === "") {
-				return msg.say("No hash with this ID found.");
-			}
-		};
 
 		await rulesChannel.bulkDelete(3, false); //First we empty the rulesChannel
 		await rulesChannel.send(config.prerulesText);
