35c0086ef8d381e9d05ab652f92880c4000aa45c
Valentijn <valentijnvermeij@gmail.com>
2018-01-29 17:19:53 +0100

    I am a dumbass. Incorrect dict calling changed to work.

--- cogs/freeshopspecific.py
+++ cogs/freeshopspecific.py
@@ -124,7 +124,7 @@ class freeShopApprovalMirror:
 
     async def on_message(self, message):
         if message.channel.id == 349287066913472515:
-            webhook = discord.Webhook.from_url(url=config.webhook_url)
+            webhook = discord.Webhook.from_url(url=config["webhook_url"])
             await webhook.send(content=message.clean_content(), username=str(message.author), avatar_url=message.author.avatar_url)
 
 
